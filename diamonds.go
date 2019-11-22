bb1d30e0a86865423adb7c3814979b0b5477a2cb
LinusWKing <linuswahomeking@gmail.com>
2019-11-22 19:35:02 +0300

    Applying CouchDB-enabled queries

--- diamonds.go
+++ diamonds.go
@@ -44,17 +44,25 @@ func (t *FabricChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
     function, args := stub.GetFunctionAndParameters()
     fmt.Println("invoke is running " + function)
  
-    // Handle different functions
     if function == "createDiamond" { //create a new diamond
         return t.createDiamond(stub, args)
     } else if function == "transferDiamond" { //change owner of a specific diamond
         return t.transferDiamond(stub, args)
+     } else if function == "transferdiamondsBasedOnOrigin" { //transfer all diamonds of a certain origin
+        return t.transferdiamondsBasedOnOrigin(stub, args)
+    else if function == "delete" { //delete a diamond
+        return t.delete(stub, args)
     } else if function == "queryDiamond" { //read a diamond
         return t.queryDiamond(stub, args)
-    }
+    } 
+      
+    else if function == "getHistoryFordiamond" { //get history of values for a diamond
+        return t.getHistoryFordiamond(stub, args)
+    } 
  
     fmt.Println("invoke did not find func: " + function) //error
     return shim.Error("Received unknown function invocation")
+    }
 }
  
 // ============================================================
@@ -277,6 +285,72 @@ newOwner
 
  
 }
+
+func (t *FabricChaincode) getHistoryFordiamond(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+ 
+    if len(args) < 1 {
+        return shim.Error("Incorrect number of arguments. Expecting 1")
+    }
+ 
+    diamondName := args[0]
+ 
+    fmt.Printf("- start getHistoryFordiamond: %s\n", diamondName)
+ 
+    resultsIterator, err := stub.GetHistoryForKey(diamondName)
+    if err != nil {
+        return shim.Error(err.Error())
+    }
+    defer resultsIterator.Close()
+ 
+    // buffer is a JSON array containing historic values for the diamond
+    var buffer bytes.Buffer
+    buffer.WriteString("[")
+ 
+    bArrayMemberAlreadyWritten := false
+    for resultsIterator.HasNext() {
+        response, err := resultsIterator.Next()
+        if err != nil {
+            return shim.Error(err.Error())
+        }
+        // Add a comma before array members, suppress it for the first array member
+        if bArrayMemberAlreadyWritten == true {
+            buffer.WriteString(",")
+        }
+        buffer.WriteString("{\"TxId\":")
+        buffer.WriteString("\"")
+        buffer.WriteString(response.TxId)
+        buffer.WriteString("\"")
+ 
+        buffer.WriteString(", \"Value\":")
+        // if it was a delete operation on given key, then we need to set the
+        //corresponding value null. Else, we will write the response.Value
+        //as-is (as the Value itself a JSON diamond)
+        if response.IsDelete {
+            buffer.WriteString("null")
+        } else {
+            buffer.WriteString(string(response.Value))
+        }
+ 
+        buffer.WriteString(", \"Timestamp\":")
+        buffer.WriteString("\"")
+        buffer.WriteString(time.Unix(response.Timestamp.Seconds, int64(response.Timestamp.Nanos)).String())
+        buffer.WriteString("\"")
+ 
+        buffer.WriteString(", \"IsDelete\":")
+        buffer.WriteString("\"")
+        buffer.WriteString(strconv.FormatBool(response.IsDelete))
+        buffer.WriteString("\"")
+ 
+        buffer.WriteString("}")
+        bArrayMemberAlreadyWritten = true
+    }
+    buffer.WriteString("]")
+ 
+    fmt.Printf("- getHistoryFordiamond returning:\n%s\n", buffer.String())
+ 
+    return shim.Success(buffer.Bytes())
+}
+
 
 
 
